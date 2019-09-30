38697ffddbd0859e8352ed00df80a0a99295a833
iamntbb <54301294+iamntbb@users.noreply.github.com>
2019-09-30 14:24:30 +0530

    Update verf.c

--- verf.c
+++ verf.c
@@ -3,31 +3,35 @@
 #include "verf.h"
 
 int main()
-{//assigning the variables
-  double sum = 0,q,k,ratio1,ratio2,num,n = 9999999;
+{
+//assigning the variables
+  double sum = 0,q,k,ratio1,ratio2,ratio3,ratio4,num,n = 9999999;
   int i = 1;
-//Part1=numerator/summation
+//numerator:summation
   while (i <= n)
     {
       q = cbrt (i);
       sum = sum + q;
       i = i + 1;
     }
-//Part1=denominator
+//denominator
   k = pow (n, 1.3333);
-//Part1
+//final answer
   num = sum / k;
-
-//Final Answer
-//Theoretical Ratio=54
+ 
 ratio1=num/den(8);
 ratio2=num/den(-9);
+ratio3=num/den(-6);
+ratio4=num/den(7);
+
 //Theoretical Numerator=0.75
-//Theoretical Denominator=0.0138888888(1/72)
+//Theoretical Denominator=0.0138888888
 printf("Numerator=%lf\n",num);
 printf("Denominator1=%lf\n",den(8));
 printf("Denominator2=%lf\n",den(-9));
 printf("Answer1=%.6lf\n",ratio1);
-printf("Answer2=%.6lf",ratio2);
+printf("Answer2=%.6lf\n",ratio2);
+printf("Wrong Answer3=%.6lf\n",ratio3);
+printf("Wrong Answer4=%.6lf\n",ratio4);
 return 0;
 }
